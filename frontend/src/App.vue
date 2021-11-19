d3e36ac47fe6038659363dd01a814a81ae66b51c
Dmitry <dmitry.shashkov@orioninc.com>
2021-11-19 23:39:36 +0300

    use vite and element+ for frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,56 +1,10 @@
-<template>
-  <div class="card">
-    <h1>Accounts:</h1>
-    <div class="p-d-flex" v-for="account in accounts" v-bind:key="account.id">
-      {{ account.name }}
-    </div>
-    <InputText type="text" v-model="newAccount" />
-    <Button label="Add new" @click="addAccount" />
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Layout from './components/Layout.vue';
+</script>
 
-    <div class="card">
-      <div class="flex card-container">
-        <div class="flex-1 h-4rem text-white font-bold text-center p-4 border-round">1</div>
-        <div class="flex-1 h-4rem bg-indigo-500 text-white font-bold text-center p-4 border-round mx-4">2</div>
-        <div class="flex-1 h-4rem bg-indigo-500 text-white font-bold text-center p-4 border-round">3</div>
-      </div>
-    </div>
-  </div>
+<template>
+  <Layout></Layout>
 </template>
 
-<script>
-import axios from 'axios';
-export default {
-  data() {
-    return {
-      accounts: [],
-      newAccount: '',
-    };
-  },
-
-  methods: {
-    async getData() {
-      try {
-        const response = await axios.get('http://localhost:4000/accounts');
-        this.accounts = response.data;
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    async addAccount() {
-      try {
-        await axios.post('http://localhost:4000/accounts', {
-          name: this.newAccount,
-        });
-        this.getData();
-        this.newAccount = '';
-      } catch (error) {
-        console.log(error);
-      }
-    },
-  },
-
-  created() {
-    this.getData();
-  },
-};
-</script>
---
+++ frontend/src/components/Layout.vue
@@ -0,0 +1,91 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { Message, Menu, Setting } from '@element-plus/icons'
+
+export default defineComponent({
+    components: {
+        Message,
+        Setting,
+        'icon-menu': Menu,
+    },
+    setup() {
+        const item = {
+            date: '2016-05-02',
+            name: 'Tom',
+            address: 'No. 189, Grove St, Los Angeles',
+        }
+
+        const tableData = ref(Array(20).fill(item))
+
+        return {
+            tableData,
+        }
+    },
+})
+</script>
+
+<template>
+    <el-container style="border: 1px solid rgba(238, 238, 238, 0.267)">
+        <el-header style="text-align: right; font-size: 12px">
+            <el-dropdown>
+                <el-icon style="margin-right: 15px">
+                    <setting />
+                </el-icon>
+                <template #dropdown>
+                    <el-dropdown-menu>
+                        <el-dropdown-item>View</el-dropdown-item>
+                        <el-dropdown-item>Add</el-dropdown-item>
+                        <el-dropdown-item>Delete</el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
+            <span>Tom</span>
+        </el-header>
+
+        <el-container>
+            <el-aside width="300px" style="background-color: rgb(238, 241, 246)">
+                <el-menu :default-openeds="['1']">
+                    <el-sub-menu index="1">
+                        <template #title>
+                            <el-icon>
+                                <message />
+                            </el-icon>Navigator One
+                        </template>
+                        <el-menu-item-group>
+                            <template #title>Group 1</template>
+                            <el-menu-item index="1-1">Option 1</el-menu-item>
+                            <el-menu-item index="1-2">Option 2</el-menu-item>
+                        </el-menu-item-group>
+                        <el-menu-item-group title="Group 2">
+                            <el-menu-item index="1-3">Option 3</el-menu-item>
+                        </el-menu-item-group>
+                        <el-sub-menu index="1-4">
+                            <template #title>Option4</template>
+                            <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
+                        </el-sub-menu>
+                    </el-sub-menu>
+                </el-menu>
+            </el-aside>
+
+            <el-main>
+                <el-table :data="tableData">
+                    <el-table-column prop="date" label="Date" width="140"></el-table-column>
+                    <el-table-column prop="name" label="Name" width="120"></el-table-column>
+                    <el-table-column prop="address" label="Address"></el-table-column>
+                </el-table>
+            </el-main>
+        </el-container>
+    </el-container>
+</template>
+
+<style>
+.el-header {
+    background-color: #b3c0d1c7;
+    color: var(--el-text-color-primary);
+    line-height: 60px;
+}
+
+.el-aside {
+    color: var(--el-text-color-primary);
+}
+</style>
